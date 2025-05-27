3c2797a09bf956b13cb256999efc722a4397a00b
caohuayu3 <caohuayu3@jd.com>
2025-05-27 16:46:12 +0800

    init

---
+++ lib/types/const.d.ts
@@ -0,0 +1,3 @@
+export declare const fetchType: string[];
+export declare const source: number[];
+//# sourceMappingURL=const.d.ts.map
---
+++ lib/types/index.d.ts
@@ -0,0 +1,9 @@
+export declare function helloWorld(): string;
+export declare function goodBye(): string;
+declare const _default: {
+    helloWorld: typeof helloWorld;
+    goodBye: typeof goodBye;
+    fetchType: string[];
+};
+export default _default;
+//# sourceMappingURL=index.d.ts.map
